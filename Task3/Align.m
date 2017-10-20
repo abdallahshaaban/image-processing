81931cc5ef85aa3caef0b2697ea2d7b9d1285dfe
Rabah Jamal <rabah9686@gmail.com>
2017-10-20 18:10:10 +0200

    Update Align.m

--- Task3/Align.m
+++ Task3/Align.m
@@ -6,42 +6,26 @@ function [AlignedImage, Corners] = Align( Image, Dw, HW)
 P = ones(3, 4);
 
 %3*4 matrix of the corresponding points on the 2ed image
-%Q = [Dw 1 Dw 1; 1 1 HW HW; 1 1 1 1];
 Q = [1 Dw 1 Dw; 1 1 HW HW; 1 1 1 1];
 
 [H W L] = size(Image);
 
-
-%detecting the markers
 for row=1:H
     for col=1:W
         if(Image(row, col, 2) == 0 && Image(row, col, 3) == 0 && Image(row, col, 1) == 224)
             P(:, 2) = [col; row; 1];
-            %disp(Image(row, col, :));
         elseif(Image(row, col, 1) == 0 && Image(row, col, 2) == 0 && Image(row, col, 3) >= 200)
             P(:, 4) = [col; row; 1];
-            %disp(Image(row, col, :));
         elseif(Image(row, col, 1) == 0 && Image(row, col, 3) == 0 && Image(row, col, 2) >= 200)
             P(:, 3) = [col; row; 1];
-            %disp(Image(row, col, :));
         elseif(Image(row, col, 3) == 0 && Image(row, col, 2) == 0 && Image(row, col, 1) == 243)
             P(:, 1) = [col; row; 1];
-            %disp(Image(row, col, :));
         end
     end
 end
 
-
 %calculating the transformation matrix
 WW = Q * P.' * inv(P * P.');
-
-
-
-AlignedImage = GeometricTransformation(Image, WW);
-Corners = Q;
-
-
-
 
 end
 
