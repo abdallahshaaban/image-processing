18dd2551abd7d990336bddcd9bc0e075966463c0
Rabah Jamal <rabah9686@gmail.com>
2017-10-21 23:01:02 +0200

    Update Intensity_Level_Slicing.m

--- Task2/Intensity_Level_Slicing.m
+++ Task2/Intensity_Level_Slicing.m
@@ -1,4 +1,4 @@
-function result = Intensity_Level_Slicing(Image, A, B, offset)
+function result = Intensity_Level_Slicing(Image, A, B, value)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -8,13 +8,12 @@ result = uint8(zeros(H, W, L));
 for row=1:H
     for col=1:W
         if(Image(row, col) >= A && Image(row, col) <= col)
-            result(row, col) = Image(row, col)+offset;
+            result(row, col) = Image(row, col)+value;
         else
             result(row, col) = Image(row, col);
         end
     end
 end
-
 
 end
 
