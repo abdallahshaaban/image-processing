efbaa20ae6f6ae3c3b9e402afc7cba185a22acbc
AbdullahMahmoud <abdullahfcis@yahoo.com>
2017-12-16 05:25:30 +0200

    .

---
+++ project/ExtractRegionsOfNumbers.m
@@ -0,0 +1,66 @@
+function [ Results ] = ExtractRegionsOfNumbers(I)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+CC = bwconncomp(I);
+bounds = regionprops(CC,'all');
+[h w] = size(bounds);
+NearestRegion = zeros(2,h);
+Visited = zeros(1,h);
+for i=1:h
+    MainX = bounds(i).BoundingBox(1);
+    MainY = bounds(i).BoundingBox(2);
+    MainWidth = bounds(i).BoundingBox(3);
+    MainHieght = bounds(i).BoundingBox(4);
+    if(~(MainHieght > 30 && MainHieght < 100 && MainWidth > 30 && MainWidth < 110 &&  bounds(i).Extent >.3 && bounds(i).Area > 500 && (max(MainHieght,MainWidth)/min(MainHieght,MainWidth))>=1 && (max(MainHieght,MainWidth)/min(MainHieght,MainWidth))<=3 ))
+        continue;
+    end
+    for j=1 : h
+        NearX = bounds(j).BoundingBox(1);
+        NearY = bounds(j).BoundingBox(2);
+        NearWidth = bounds(j).BoundingBox(3);
+        NearHieght = bounds(j).BoundingBox(4);
+        if(i == j || ~(bounds(j).Extent >.3 && bounds(j).Area > 500 && NearHieght > 30 && NearHieght < 100 && NearWidth > 30 && NearWidth < 110 )) 
+            continue;
+        end
+        if((max(NearHieght,NearWidth)/min(NearHieght,NearWidth))>=1 && (max(NearHieght,NearWidth)/min(NearHieght,NearWidth))<=3 && abs(MainHieght - NearHieght)<=10 && abs(NearY - MainY) <= NearHieght/4  && NearX - MainX>=MainWidth - 10 && NearX - MainX<=MainWidth+10)                                                                        
+            NearestRegion(:,i) = [i;j];
+            Visited(:,j) = 1;
+        end
+    end
+end
+Results = zeros(0,10);
+for i=1 : h
+    if(NearestRegion(1,i) ~= 0 && Visited(1,i) == 0)
+        k=i;
+        j=1;
+        TmpRes = zeros(1,10);
+        TmpRes(:,j) = NearestRegion(1,k);
+        Flag = 0;
+        IC=imcrop(I,bounds(NearestRegion(1,k)).BoundingBox);
+        SE = strel('square',3);
+        IC = imerode(IC,SE);
+        IC = bwareaopen(IC, 50);
+        CC = bwconncomp(IC);
+        if(CC.NumObjects == 1)
+            Flag = 1;
+        end
+        while NearestRegion(1,k) ~= 0 % && Flag
+            j = j + 1;
+            IC=imcrop(I,bounds(NearestRegion(2,k)).BoundingBox);
+            SE = strel('square',3);
+            IC = imerode(IC,SE);
+            IC = bwareaopen(IC, 50);
+            CC = bwconncomp(IC);
+            if(CC.NumObjects == 1)
+                Flag = 1;
+            end
+            TmpRes(:,j) = NearestRegion(2,k);
+            k = NearestRegion(2,k);
+        end
+        if Flag
+        Results = [Results ; TmpRes];
+        end
+    end
+end
+end
+
--- project/detectDigits.m
+++ project/detectDigits.m
@@ -11,49 +11,47 @@ I= bwareaopen(I,300);
 se = strel('disk',1);
 I = imopen(I,se);
 I= bwareaopen(I,300);
-
+Groups = ExtractRegionsOfNumbers(I);
+[h w] = size(Groups);
+% Extra code for testing
 CC = bwconncomp(I);
 bounds = regionprops(CC,'all');
-z=size(bounds);
-for i=1:size(bounds)
-    
-    width = bounds(i).BoundingBox(3);
-    height= bounds(i).BoundingBox(4);
-    
-    if(bounds(i).Area <500 || bounds(i).Area >=1500 || height <=30 || height >=100 || width <=30 || width >=110)
-        continue;
-    end;
-    
-    if( (height / width)>=1 && (height / width) <= 3&&bounds(i).Extent >.3&&bounds(i).FilledArea >500&&bounds(i).FilledArea <1600)
-
-        IC=imcrop(Image,bounds(i).BoundingBox); 
-    %    figure,imshow(IC);
-    rectangle('Position',bounds(i).BoundingBox,'edgecolor','g','linewidth',2);
+for i=1 : h
+    for j=1 : w
+        if(Groups(i,j) == 0) 
+            break;
+        end
+        rectangle('Position',bounds(Groups(i,j)).BoundingBox,'edgecolor','g','linewidth',2);
+%         IC=imcrop(I,bounds(Groups(i,j)).BoundingBox); 
+%         SE = strel('square',3);
+%         IC = imerode(IC,SE);
+%         IC = bwareaopen(IC, 50);
+%         figure , imshow(IC);
+%         CC = bwconncomp(IC);
+%         CC.NumObjects
     end
-    
 end
 
-
-
 I = 1-I ;
+Groups = ExtractRegionsOfNumbers(I);
+[h w] = size(Groups);
+% Extra code for testing
 CC = bwconncomp(I);
 bounds = regionprops(CC,'all');
-z=size(bounds);
-
-for i=1:size(bounds)
-    
-    width = bounds(i).BoundingBox(3);
-    height= bounds(i).BoundingBox(4);
-    
-    if(bounds(i).Area < 500 || bounds(i).Area >=1500 || height <=30 || height >=100 || width <=30 || width >=110 )
-        continue;
-    end;
-    
-    if( (height / width)>=0.8 && (height / width) <= 3&&bounds(i).Extent >.3&&bounds(i).FilledArea >500&&bounds(i).FilledArea <2500)
-          rectangle('Position',bounds(i).BoundingBox,'edgecolor','g','linewidth',2);
-             
+for i=1 : h
+    for j=1 : w
+        if(Groups(i,j) == 0) 
+            break;
+        end
+        rectangle('Position',bounds(Groups(i,j)).BoundingBox,'edgecolor','g','linewidth',2);
+%         IC=imcrop(I,bounds(Groups(i,j)).BoundingBox); 
+%         SE = strel('square',3);
+%         IC = imerode(IC,SE);
+%         IC = bwareaopen(IC, 50);
+%         figure , imshow(IC);
+%         CC = bwconncomp(IC);
+%         CC.NumObjects
     end
-    
 end
 
 
@@ -61,7 +59,7 @@ end
 result = I ;
 
 elseif(Im>0&&Im<6)
-    result=Im
+    result=Im;
 end
 
 end     
